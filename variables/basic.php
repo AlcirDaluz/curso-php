cedd06ffe176f4fca8b3f793fc1d79f703b20260
Alcir Daluz <alcirdaluz@protonmail.com>
2024-02-14 22:42:01 -0300

    Aula 54 - Variáveis parte 2

--- variables/basic.php
+++ variables/basic.php
@@ -22,3 +22,16 @@ echo '<br>' . $variavel;
 
 $variavel = "Agora sou uma string!"; // o interpretador do php consegue inferir o tipo da variável, php linguagem de tipagem fraca
 echo '<br>' . $variavel;
+
+// Nomes de variáveis
+$var = 'valida';
+$var2 = 'valida';
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+$vâr5 = 'valida'; // não é aconselhado utilizar acentuação na nomenclatura de variáveis
+//$6var = 'invalida';
+//$%var7 = 'invalida';
+//$var8% = 'invalida';
+
+echo '<br>';
+var_dump($_SERVER["HTTP_HOST"]);
