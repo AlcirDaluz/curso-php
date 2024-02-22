8431d5991cf744cc679f6f63aeaa9e11e84bef08
Alcir Daluz <alcirdaluz@protonmail.com>
2024-02-22 15:59:46 -0300

    Aula 70 - If/Else Parte 02

--- control/if_else.php
+++ control/if_else.php
@@ -4,3 +4,27 @@
 if (true) {
     echo "Serei impresso?";
 }
+
+if (false) {
+    echo "<br>Verdadeiro - Parte A<br>";
+    echo "Verdadeiro - Parte B<br>";
+} else {
+    echo "<br>Falso - Parte A<br>";
+    echo "Falso - Parte B<br>";
+}
+
+if (false) {
+    echo "Passo A<br>";
+} elseif (false) {
+    echo "Passo B<br>";
+} elseif (false) {
+    echo "Passo C<br>";
+} elseif (false) {
+    echo "Passo D<br>";
+} elseif (false) {
+    echo "Passo E<br>";
+} else {
+    echo "Último Passo<br>";
+}
+
+echo "Fim<br>";
