12a557930dea36fa43b036ad3cacc809c0997e1e
Alcir Daluz <alcirdaluz@protonmail.com>
2024-02-25 01:04:46 -0300

    Aula 72 - Operadores Relacionais Parte 02

--- control/relational_operators.php
+++ control/relational_operators.php
@@ -15,11 +15,33 @@ var_dump(111 != '111'); // false
 var_dump(111 !== '111'); // true
 
 echo "<p>Relacionais no If/Else</p><hr>";
-$idade = 75;
+$idade = 15;
 if ($idade < 18) {
-    echo "Menor de idade = $idade<br>";
+    echo "Menor de idade = $idade anos<br>";
 } elseif ($idade <= 65) {
-    echo "Adulto = $idade<br>";
+    echo "Adulto = $idade anos<br>";
 } else {
-    echo "Terceira idade = $idade";
+    echo "Terceira idade = $idade anos!";
 }
+
+echo '<p>Spaceship</p><hr>';
+var_dump(500 <=> 3);
+var_dump(50 <=> 50);
+var_dump(5 <=> 50);
+
+echo '<p>Valores podem ser V ou F</p><hr>';
+var_dump(!!5);
+var_dump(!!0);
+var_dump(!!"");
+var_dump(!!" ");
+?>
+
+<style>
+    p {
+        margin-bottom: 0;
+    }
+
+    hr {
+        margin-top: 0;
+    }
+</style>
