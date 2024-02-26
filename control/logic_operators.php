c970b6e0bc5cfb84d63e302c374442ec7cfb2c24
Alcir Daluz <alcirdaluz@protonmail.com>
2024-02-26 15:56:39 -0300

    Aula 75 - Operadores Lógicos Parte 02

--- control/logic_operators.php
+++ control/logic_operators.php
@@ -1,12 +1,12 @@
 <div class="title">Operadores Lógicos</div>
 
 <?php
-echo "<p>Negação Lógica</p><hr>";
+echo "<p class='divisao'>Negação Lógica</p><hr>";
 var_dump(true);
 echo '<br>';
 var_dump(!true); // not
 
-echo "<p>Tabela Verdade 'AND' (E)</p><hr>";
+echo "<p class='divisao'>Tabela Verdade 'AND' (E)</p><hr>";
 var_dump(true && true);
 var_dump(true && false);
 var_dump(false && true);
@@ -17,7 +17,7 @@ var_dump(true and false);
 var_dump(false and true);
 var_dump(false and false);
 
-echo "<p>Tabela Verdade 'OR' (OU)</p><hr>";
+echo "<p class='divisao'>Tabela Verdade 'OR' (OU)</p><hr>";
 var_dump(true || true);
 var_dump(true || false);
 var_dump(false || true);
@@ -28,7 +28,7 @@ var_dump(true or false);
 var_dump(false or true);
 var_dump(false or false);
 
-echo "<p>Tabela Verdade 'XOR' (OU Exclusivo)</p><hr>";
+echo "<p class='divisao'>Tabela Verdade 'XOR' (OU Exclusivo)</p><hr>";
 var_dump(true xor true);
 var_dump(true xor false);
 var_dump(false xor true);
@@ -38,15 +38,23 @@ var_dump(true != true);
 var_dump(true != false);
 var_dump(false != true);
 var_dump(false != false);
-?>
 
-<style>
-    p {
-        margin-bottom: 0;
-        font-weight: bold;
-    }
+echo "<p class='divisao'>Exemplo</p><hr>";
+$idade = 65;
+$sexo = 'M';
 
-    hr {
-        margin-top: 0;
-    }
-</style>
+$pagouPrevidencia = true;
+
+$criterioMasculino = ($idade >= 65 && $sexo === 'M');
+$criterioFeminino = ($idade >= 60 && $sexo === 'F');
+$atingiuCriterio = $criterioMasculino || $criterioFeminino;
+$podeSeAposentar = $pagouPrevidencia && $atingiuCriterio;
+echo "Pode se aposentar -> $podeSeAposentar<br>";
+
+if ($idade >= 60 && $sexo === 'F') {
+    echo "Pode se aposentar -> $sexo";
+} elseif ($idade >= 65 && $sexo === 'M') {
+    echo "Pode se aposentar -> $sexo";
+} else {
+    echo 'Vai ter que trabalhar mais um pouco...';
+}
